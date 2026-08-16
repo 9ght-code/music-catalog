7c9795c4ecc8db0b13b4bd3d041fadc0b4530e7c
9ght-code <deadm9904@gmail.com>
2026-08-16 15:08:49 +0500

    Эндпоинт GET /artists/:id с обработкой 404 - cask.Response[ujson.Value] + match по Option - Несуществующий id → 404 с JSON-ошибкой

--- music-catalog/src/main/scala/Main.scala
+++ music-catalog/src/main/scala/Main.scala
@@ -7,4 +7,11 @@ object Main extends cask.MainRoutes:
 
     @cask.get("/artists")
     def artists() = repo.findAll().map(a => ujson.Obj("id" -> a.id, "name" -> a.name))
+
+    @cask.get("/artists/:id")
+    def artistById(id: Int): cask.Response[ujson.Value] =
+        repo.findById(id) match
+            case Some(a) => cask.Response(ujson.Obj("id" -> a.id, "name" -> a.name))
+            case None => cask.Response(ujson.Obj("error" -> "Not Found"), statusCode = 404)
+
     initialize()
